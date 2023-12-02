4a51f12db09ac7f8a61a12bca68c1e5b5ed84266
jalanprakhar <iit2020200@iiita.ac.in>
2023-12-02 22:35:59 +0530

    hd

--- server/routes/auth/auth.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { login, signup,getSingleUser } = require("./auth.controller");
-const authRouter = express.Router();
-authRouter.post("/login/", login);
-authRouter.post("/signup/", signup);
-authRouter.get("/:id", getSingleUser);
-module.exports = authRouter;
--- server/routes/review/review.router.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { addReview, getReview } = require("./review.controller");
-const reviewRouter = express.Router();
-reviewRouter.post("/", addReview);
-reviewRouter.get("/:id", getReview);
-module.exports = reviewRouter;
