d8ed119b29bf157219fa028af761febf03804bf5
jalanprakhar <iit2020200@iiita.ac.in>
2023-12-02 07:41:23 +0530

    Final deployment

--- server/app.js
+++ server/app.js
@@ -16,8 +16,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/v1", apiRouter);
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 module.exports = app;
